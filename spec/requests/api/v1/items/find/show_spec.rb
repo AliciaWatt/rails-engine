846dbe6f53a2c3734f473a40a150cc8060bea1f5
Alicia Watt <alicia.watt.aw@gmail.com>
2022-05-18 21:02:10 -0600

    refactor tests for updated response format

--- spec/requests/api/v1/items/find/show_spec.rb
+++ spec/requests/api/v1/items/find/show_spec.rb
@@ -1,40 +1,55 @@
 require "rails_helper"
+
 RSpec.describe "api/v1/items/find" do
   context "happy paths - items exist" do
     let!(:item_1) { create(:item, name: "Baseball Bat", unit_price: 7.99) }
     let!(:item_2) { create(:item, name: "Cricket Bat", unit_price: 107.99) }
     let!(:item_3) { create(:item, name: "Zebra", unit_price: 2.99) }
+
     context "search by name when items exist" do
       let(:query) { "?name=bat" }
+
       before(:each) do
         get "/api/v1/items/find#{query}"
       end
+
       let!(:json) { JSON.parse(response.body, symbolize_names: true) }
+
       it "returns a status code of 200" do
         expect(response).to have_http_status(200)
       end
+
       it "returns the expected data Hash" do
         expect(json).to be_a(Hash)
         expect(json).to have_key(:data)
+
         expect(json[:data]).to be_a(Hash)
       end
+
       it "returns the expected hash structure for the item" do
         expect(json[:data]).to be_a(Hash)
+
         expect(json[:data]).to have_key(:id)
         expect(json[:data][:id]).to be_a(String)
+
         expect(json[:data]).to have_key(:type)
         expect(json[:data][:type]).to be_a(String)
         expect(json[:data][:type]).to eq("item")
+
         expect(json[:data]).to have_key(:attributes)
         expect(json[:data][:attributes]).to be_a(Hash)
       end
+
       it "returns the expected item attributes" do
         expect(json[:data][:attributes]).to have_key(:name)
         expect(json[:data][:attributes][:name]).to be_a(String)
+
         expect(json[:data][:attributes]).to have_key(:description)
         expect(json[:data][:attributes][:description]).to be_a(String)
+
         expect(json[:data][:attributes]).to have_key(:unit_price)
         expect(json[:data][:attributes][:unit_price]).to be_a(Float)
+
         expect(json[:data][:attributes]).to have_key(:merchant_id)
         expect(json[:data][:attributes][:merchant_id]).to be_a(Integer)
       end
@@ -101,13 +116,25 @@ RSpec.describe "api/v1/items/find" do
         expect(response).to have_http_status(200)
       end
 
-      # it "returns an empty response" do
-      #   json = JSON.parse(response.body, symbolize_names: true)
-      #   expect(json).to be_a(Hash)
-      #   expect(json).to have_key(:data)
-      #   expect(json[:data]).to be_a(Hash)
-      #   expect(json[:data][:error]).to match(/Bad request/)
-      # end
+      it "returns an empty response" do
+        json = JSON.parse(response.body, symbolize_names: true)
+        expect(json).to be_a(Hash)
+        expect(json).to have_key(:data)
+        expect(json[:data]).to be_a(Hash)
+
+        expect(json[:data]).to have_key(:id)
+        expect(json[:data][:id]).to eq(nil)
+
+        expect(json[:data]).to have_key(:type)
+        expect(json[:data][:type]).to be_a(String)
+        expect(json[:data][:type]).to eq("item")
+
+        expect(json[:data]).to have_key(:attributes)
+        expect(json[:data][:attributes]).to be_a(Array)
+        expect(json[:data][:attributes]).to eq([])
+
+        expect(json[:error]).to match(/No records found/)
+      end
     end
 
     context "search fields are empty" do
@@ -124,8 +151,10 @@ RSpec.describe "api/v1/items/find" do
         json = JSON.parse(response.body, symbolize_names: true)
         expect(json).to be_a(Hash)
         expect(json).to have_key(:data)
-        # expect(json[:data]).to be_a(Hash)
-        # expect(json[:data][:error]).to match(/Bad request/)
+        expect(json[:data]).to eq([])
+
+        expect(json).to have_key(:error)
+        expect(json[:error]).to match(/Bad request/)
       end
     end
 
@@ -143,8 +172,10 @@ RSpec.describe "api/v1/items/find" do
         json = JSON.parse(response.body, symbolize_names: true)
         expect(json).to be_a(Hash)
         expect(json).to have_key(:data)
-        # expect(json[:data]).to be_a(Hash)
-        # expect(json[:data][:error]).to match(/Bad request/)
+        expect(json[:data]).to eq([])
+
+        expect(json).to have_key(:error)
+        expect(json[:error]).to match(/Bad request/)
       end
     end
 
@@ -162,8 +193,10 @@ RSpec.describe "api/v1/items/find" do
         json = JSON.parse(response.body, symbolize_names: true)
         expect(json).to be_a(Hash)
         expect(json).to have_key(:data)
-        # expect(json[:data]).to be_a(Hash)
-        # expect(json[:data][:error]).to match(/Bad request/)
+        expect(json[:data]).to eq([])
+
+        expect(json).to have_key(:error)
+        expect(json[:error]).to match(/Bad request/)
       end
     end
 
@@ -173,7 +206,7 @@ RSpec.describe "api/v1/items/find" do
         get "/api/v1/items/find?name=zebra&max_price=50&max_price=500"
       end
 
-      it "returns status code 400 and empty" do
+      it "returns status code 400" do
         expect(response).to have_http_status(400)
       end
 
@@ -181,8 +214,10 @@ RSpec.describe "api/v1/items/find" do
         json = JSON.parse(response.body, symbolize_names: true)
         expect(json).to be_a(Hash)
         expect(json).to have_key(:data)
-        # expect(json[:data]).to be_a(Hash)
-        # expect(json[:data][:error]).to match(/Bad request/)
+        expect(json[:data]).to eq([])
+
+        expect(json).to have_key(:error)
+        expect(json[:error]).to match(/Bad request/)
       end
     end
   end
