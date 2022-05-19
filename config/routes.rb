49c25133bd15b2e5b5969e0417500a0585e8086c
Alicia Watt <alicia.watt.aw@gmail.com>
2022-05-18 22:19:15 -0600

    model test: merchant find_all class method

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
       namespace :items do
         get "/find", to: "find#show"
       end
+
+      namespace :merchants do
+        get "/find_all", to: "find_all#index"
+      end
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: :merchant_items
       end
